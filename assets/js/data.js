afde9608f9ec6d7bc069c988b793f5a076a05c0d
devjoaocamargo <joaovscamargo01@gmail.com>
2022-09-06 00:10:32 -0300

    modal portoflio

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,33 +1,67 @@
+// {
+//    tools: {
+//       html: '',
+//       css: '',
+//       js: '',
+//       php: '',
+//       mongodb: '',
+//       mysql: '',
+//       nodejs: '',
+//       react: '',
+//       sass: '',
+//       tailwind: '',
+//    }
+// },
+
 const portDataWeb = [
    {
       name: "Portfolio",
       img: "./assets/img/portfolioImg.png",
       description: "Meu portofolio",
       link: "https://devjoaocamargo.github.io/",
-      category: "web-application"
+      category: "web-application",
+      tools: {
+         html: './assets/img/icons/icons8-html-5-48.svg',
+         css: './assets/img/icons/icons8-css3-48.svg',
+         js: './assets/img/icons/icons8-javascript-48.svg',
+      },
    },
    {
       name: "Player De Musica",
       img: "./assets/img/playerMusicImg.png",
       description: "Fiz um player de musica para treinar CSS e principalmente JS",
       link: "https://player-music-anime.vercel.app/",
-      category: "web-application"
+      category: "web-application",
+      tools: {
+         html: './assets/img/icons/icons8-html-5-48.svg',
+         css: './assets/img/icons/icons8-css3-48.svg',
+         js: './assets/img/icons/icons8-javascript-48.svg',
+      },
    },
    {
       name: "Gerador de Senhas",
       img: "./assets/img/geradorSenhasImg.png",
       description: "Fiz esse gerador de senhas treinar JS",
       link: "https://gerador-senhas-iota.vercel.app/",
-      category: "web-application"
+      category: "web-application",
+      tools: {
+         html: './assets/img/icons/icons8-html-5-48.svg',
+         css: './assets/img/icons/icons8-css3-48.svg',
+         js: './assets/img/icons/icons8-javascript-48.svg',
+      },
    },
    {
       name: "Pokedex",
       img: "./assets/img/pokedex.png",
       description: "Fiz esse pokedex para treinar fetch do JS",
       link: "https://pokedex-rosy-two.vercel.app/",
-      category: "web-application"
+      category: "web-application",
+      tools: {
+         html: './assets/img/icons/icons8-html-5-48.svg',
+         css: './assets/img/icons/icons8-css3-48.svg',
+         js: './assets/img/icons/icons8-javascript-48.svg',
+      },
    },
-
 ];
 
 const portDataFigma = [
@@ -58,7 +92,7 @@ const portDataEcommerce = [
       img: "./assets/img/breve.png",
       description: "breve",
       link: "#",
-      category: "e-commerce"
+      category: "e-commerce",
    },
 
 ];
--- assets/js/global.js
+++ assets/js/global.js
@@ -11,6 +11,34 @@ typeWriter(titulo);
 //animação maquina de escrever end
 
 
+const abrirModal = () => {
+
+   document.querySelector('.porModal').style.opacity = 0;
+   document.querySelector('.porModal').style.display = 'flex';
+   setTimeout(() => {
+      document.querySelector('.porModal').style.opacity = 1;
+   }, 150);
+}
+
+const fecharModal = () => {
+   document.querySelector('.porModal').style.opacity = 0;
+   setTimeout(() => {
+      document.querySelector('.porModal').style.display = 'none'
+   }, 500);
+}
+
+const botoesFechar = () => {
+   //btn fechar modal
+   document.querySelector('.closeModal').addEventListener('click', () => {
+      document.querySelector('.porModal').style.display = 'none';
+   })
+
+   //  seleciona('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
+   //      seleciona('.pizzaWindowArea').style.display = 'none';
+   //  })
+}
+
+
 //preencher os dados dos card do portfolio start
 const preencherDados = (cardItem, item) => {
    cardItem.querySelector('div').setAttribute('data-category', item.category);
@@ -20,6 +48,23 @@ const preencherDados = (cardItem, item) => {
 }
 //preencher os dados dos card do portfolio end
 
+const preencherDadosModal = (item) => {
+   document.querySelector('.porModal .titleModal').innerHTML = item.name;
+   document.querySelector('.bannerItem img').src = item.img;
+   document.querySelector('.modalList .html img').src = item.tools.html;
+   document.querySelector('.modalList .css img').src = item.tools.css;
+   document.querySelector('.modalList .js img').src = item.tools.js;
+   document.querySelector('.modalList .php img').src = item.tools.php;
+   document.querySelector('.modalList .react img').src = item.tools.react;
+   document.querySelector('.modalList .node img').src = item.tools.node;
+   document.querySelector('.modalList .sass img').src = item.tools.sass;
+   document.querySelector('.modalList .tailwind img').src = item.tools.tailwind;
+   document.querySelector('.modalList .mongodb img').src = item.tools.mongodb;
+   document.querySelector('.modalList .mysql img').src = item.tools.mysql;
+   document.querySelector('.porModal .descriptionModal').innerHTML = item.description;
+
+}
+
 //clone do data do Portfolio start
 portDataWeb.map(item => {
    let cardItem = document.querySelector('.porbox_models .category').cloneNode(true);
@@ -27,6 +72,20 @@ portDataWeb.map(item => {
    document.querySelector('.portBox').append(cardItem)
    // console.log(boxItem)
    preencherDados(cardItem, item)
+
+   cardItem.querySelector('.openModal').addEventListener('click', (e) => {
+      e.preventDefault();
+
+      //abrir modal
+      abrirModal();
+
+      //preencher os dados da modal
+      preencherDadosModal(item);
+      console.log(item)
+   })
+
+   //fechar a modal no cancel e no voltar
+   botoesFechar()
 });
 //clone do data do Portfolio end
 
